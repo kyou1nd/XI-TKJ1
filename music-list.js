04ebc4fa820974835ad1d87039046930ead7ab6c
kyou1nd <nnddrra1@gmail.com>
2026-08-29 13:28:21 +0700

    Add files via upload

--- music-list.js
+++ music-list.js
@@ -5,28 +5,28 @@ window.LOCAL_MUSIC = [
     name: "Please Don't Call - Bleachers",
     artist: "Bleachers",
     src: "assets/music/Eid-Adha%2C%20Please%20Don't%20Call%20-%20Bleachers.mp3",
-    cover: "assets/music/covers/please-dont-call.svg",
+    cover: "assets/music/covers/please-dont-call.jpeg",
     type: "audio/mpeg"
   },
   {
     name: "Beautiful (feat. Camila Cabello)",
     artist: "Bazzi",
     src: "assets/music/Beautiful%20(feat.%20Camila%20Cabello).mp3",
-    cover: "assets/music/covers/beautiful.svg",
+    cover: "assets/music/covers/beautiful.jpeg",
     type: "audio/mpeg"
   },
   {
     name: "Dunia Yang Nanti",
     artist: "Raim Laode",
     src: "assets/music/Dunia%20Yang%20Nanti%20-%20Raim%20Laode.mp3",
-    cover: "assets/music/covers/dunia-yang-nanti.svg",
+    cover: "assets/music/covers/dunia-yang-nanti.jpeg",
     type: "audio/mpeg"
   },
   {
     name: "Blank Space",
     artist: "Taylor Swift",
     src: "assets/music/Blank%20Space%20-%20Taylor%20Swift.mp3",
-    cover: "assets/music/covers/blank-space.svg",
+    cover: "assets/music/covers/blank-space.jpeg",
     type: "audio/mpeg"
   }
 ];
