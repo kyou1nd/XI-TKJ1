cb489cc0a1534e549500a595ade7dce3504eb4d4
kyou1nd <nnddrra1@gmail.com>
2026-08-29 12:22:36 +0700

    Add files via upload

--- drive-config.js
+++ drive-config.js
@@ -1,6 +1,1 @@
-/*
- * GOOGLE DRIVE ABSENSI
- * Isi URL Web App Google Apps Script milik kamu setelah deploy.
- * Kosongkan jika ingin mode lokal saja.
- */
-window.DRIVE_UPLOAD_URL = "https://script.google.com/macros/s/AKfycbwzymG4bR-K7n8te5uWW9-ekeQpf6Xh50TjKeKhwNnT6zUDmEyXcU2KqZCsI0vNzGgl/exec";
+window.DRIVE_UPLOAD_URL = 'https://script.google.com/macros/s/AKfycbwzymG4bR-K7n8te5uWW9-ekeQpf6Xh50TjKeKhwNnT6zUDmEyXcU2KqZCsI0vNzGgl/exec';
