c0df7888206f535d260d8978de912ed805b44423
kyou1nd <nnddrra1@gmail.com>
2026-08-31 18:45:53 +0700

    Add files via upload

--- drive-config.js
+++ drive-config.js
@@ -2,6 +2,6 @@
  * BACKEND ABSENSI XI TKJ 1
  * Satu URL Apps Script dipakai sebagai bridge Google Drive + Google Sheets.
  */
-window.DRIVE_UPLOAD_URL = "https://script.google.com/macros/s/AKfycbwzymG4bR-K7n8te5uWW9-ekeQpf6Xh50TjKeKhwNnT6zUDmEyXcU2KqZCsI0vNzGgl/exec";
+window.DRIVE_UPLOAD_URL = "https://script.google.com/macros/s/AKfycbxhlItK5pdaFCw4_h00EKbeGG9B0qwD8kpymxPR1eG-6sI2dovvJwDsWwYhPb3eo8ap/exec";
 window.MANUAL_ATTENDANCE_URL = window.DRIVE_UPLOAD_URL;
-// PENTING: setelah Deploy Web App baru, ganti URL di atas dengan URL /exec deployment terbaru.
+// URL Web App aktif untuk backend absensi: deployment /exec yang dipasang di bawah.
